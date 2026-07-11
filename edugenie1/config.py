d8638020e56f3cb85d08594aa49f4f70e86e5fa0
jayasri37 <jayasrimarachu2006@gmail.com>
2026-07-11 10:09:22 +0530

    Delete edugenie1 directory

--- edugenie1/config.py
+++
@@ -1,8 +0,0 @@
-from dotenv import load_dotenv
-import os
-
-# Load environment variables from .env (if present)
-load_dotenv()
-
-# Public config values
-GEMINI_API_KEY = os.getenv("GEMINI_API_KEY", "")
